7a6ee9fa9336f27380e32b2a88888c34d7b0a8df
Jinnie-kim <hjk7639@gmail.com>
2022-06-02 13:38:20 +0900

    JS
    memo를 입력하고 엔터를 눌렸을 때 개별 리스트 요소를 만들고 추가하는 함수 생성

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,27 @@ const memoInputForm = document.querySelector('.memopad-input');
 let memoInput = memoInputForm.querySelector('input');
 const memoListPage = document.querySelector('.page-lists');
 
+function paintMemo(newMemo) {
+    const memoList = document.createElement('li');
+    const memoListButton = document.createElement('button');
+    const memoListButtonIcon = document.createElement('i');
+    const memoListContent = document.createElement('strong');
+    memoList.setAttribute('class', 'page-list');
+    memoListButton.setAttribute('type', 'button');
+    memoListButtonIcon.setAttribute('class','fa-solid fa-check');
+    memoListContent.innerText = newMemo;
+    memoListButton.appendChild(memoListButtonIcon);
+    memoList.appendChild(memoListButton);
+    memoList.appendChild(memoListContent);
+    memoListPage.appendChild(memoList);
+}
 
 function handleMemoSubmit(event) {
     event.preventDefault();
     // input의 value를 새로운 변수에 복사 
     const newMemo = memoInput.value;
     memoInput.value = '';
+    paintMemo(newMemo);
 }
 
 memoInputForm.addEventListener('submit', handleMemoSubmit);
