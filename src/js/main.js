a92a554156c72055d08bb7519c7bfa5e0de5f2b2
Jinnie-kim <hjk7639@gmail.com>
2022-06-02 13:06:30 +0900

    JS
    memo 제출을 핸들링하는 함수 이름 변경 handleTodoSubmit -> handleMemoSubmit

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,13 @@ const memoInputForm = document.querySelector('.memopad-input');
 let memoInput = memoInputForm.querySelector('input');
 const memoListPage = document.querySelector('.page-lists');
 
-function handleToDoSubmit(event) {
+
+function handleMemoSubmit(event) {
     event.preventDefault();
     // input의 value를 새로운 변수에 복사 
     const newMemo = memoInput.value;
     memoInput.value = '';
 }
 
-memoInputForm.addEventListener('submit', handleToDoSubmit);
+memoInputForm.addEventListener('submit', handleMemoSubmit);
 
